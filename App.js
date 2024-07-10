61940718297ae0449e1c8151651a8615a8a9ed0d
Aleksej Vidi─ç <aleksej.vidic@student.etf.unibl.org>
2024-07-10 07:11:47 +0200

    Commit project

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ViroARSceneNavigator, ViroARScene, ViroImage } from '@reactvision/react-viro';
 
+const MyScene = () => {
+  return (
+    <ViroARScene>
+      <ViroImage 
+        source={require("./assets/icon.png")}
+        position={[0, 0, -1]}
+        height={0.5}
+        width={0.5}
+      />
+    </ViroARScene>
+  );
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ViroARSceneNavigator 
+      style={{
+        flex: 1
+      }}
+      autofocus={true}
+      initialScene={{
+        scene: MyScene
+      }}
+    />
   );
 }
 
